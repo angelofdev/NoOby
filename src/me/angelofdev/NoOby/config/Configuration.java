046c0b6abd00873eff07bfafc5f38a880ff3f87d
angelofdev <poya.yahyaviazad@gmail.com>
2012-06-22 23:25:13 +1000

    Update master

--- src/me/angelofdev/NoOby/config/Configuration.java
+++ src/me/angelofdev/NoOby/config/Configuration.java
@@ -1,3 +1,23 @@
+/*
+ *  NoOby: Makes Obsidian break like Glass but makes Glass & other 
+ *  			 unobtainable blocks to drop like normal blocks.
+ *  Copyright (C) 2012  M.Y.Azad
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *   
+ */
+
 package me.angelofdev.NoOby.config;
 
 import java.io.FileNotFoundException;
