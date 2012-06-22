76aba8516ba8defa769b2cc08c7aaa50cc6f3b83
angelofdev <poya.yahyaviazad@gmail.com>
2012-06-22 21:14:25 +1000

    Removed unused code

--- src/me/angelofdev/NoOby/config/Config.java
+++ src/me/angelofdev/NoOby/config/Config.java
@@ -1,12 +1,10 @@
 package me.angelofdev.NoOby.config;
 
-import java.util.Arrays;
-
 public class Config {
 
 	public static void set(){
-		if (!Configuration.config.contains("Rpg.reminder")) {
-			Configuration.config.set("Rpg.reminder", true);
+		if (!Configuration.config.contains("Rpg.messages")) {
+			Configuration.config.set("Rpg.messages", true);
 		}
 		if (!Configuration.config.contains("Rpg.Glass hurts hands")) {
 			Configuration.config.set("Rpg.Glass hurts bare hands", true);
@@ -38,24 +36,8 @@ public class Config {
 		if(!Configuration.config.contains("Chance.snow")) {
 			Configuration.config.set("Chance.snow",10);
 		}
-		if(!Configuration.config.contains("Gamemode Force.players")) {
-			Configuration.config.set("Gamemode Force.players", Arrays.asList(
-					"##admin",
-					"##mod"));
-		}
-		if(!Configuration.config.contains("Worlds on Enter force Survival.worlds")) {
-			Configuration.config.set("Worlds on Enter force Survival.worlds", Arrays.asList(
-					"#world",
-					"#nether"));
-		}
-		if(!Configuration.config.contains("Worlds on Enter force Creative.worlds")) {
-			Configuration.config.set("Worlds on Enter force Creative.worlds", Arrays.asList(
-					"#world",
-					"#nether"));
-		}
 		if(!Configuration.config.contains("Debug.enabled")) {
 			Configuration.config.set("Debug.enabled", false);
 		}
 	}
 }
-
